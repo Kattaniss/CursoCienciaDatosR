36fcea630390892bf8a7f7ac9cbdb85bd2832629
Francisco Charte Ojeda <francisco@fcharte.com>
2016-03-01 17:14:43 +0100

    Configure the system installing the needed packages

---
+++ instalaPaquetes.R
@@ -0,0 +1,23 @@
+install.packages("caret")
+# LINUX: system('wajig install libgtk2.0-dev') # Use 'sudo apt-get install wajig' at the command line if needed, then install Gtk headers
+install.packages('RGtk2')
+install.packages("rattle")
+install.packages("neuralnet")
+install.packages("autoencoder")
+install.packages("ggplot2")
+install.packages("h2o", dependencies = c("Depends", "Suggests"))
+
+library(rattle)  # WINDOWS: Choose to install Gtk
+rattle()
+
+library(h2o)
+localH2O <- h2o.init() # It should indicate that H2O is initialized, as well as the Java VM version
+
+# Download https://github.com/fcharte/CursoCienciaDatosR/blob/master/data/datosTrabajo.RData and save it in working directory
+
+# Look for the previous file at the "Files" panel, and click it to load it. All the data should appear in the Environment tab
+
+library(caret)
+library(neuralnet)
+library(autoencoder)
+library(ggplot2)
