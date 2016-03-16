c3468a881f54ffefe4613f45c1343561ca462904
Francisco Charte Ojeda <francisco@fcharte.com>
2016-03-16 13:04:20 +0100

    Hierarchical and model-based clustering exercises

--- ejerciciosClustering.R
+++ ejerciciosClustering.R
@@ -28,3 +28,38 @@ ggplot(SSE, aes(Grupos, SSE)) + geom_line() + scale_x_continuous(breaks = 1:10)
 clusters <- kmeans(datos, pamk(datos)$nc)
 ggplot(iris, aes(Petal.Length, Petal.Width)) + geom_point(aes(color = as.factor(clusters$cluster), shape = iris$Species), size = 5)
 clusplot(iris[, -5], clusters$cluster, color = TRUE, lines = 0)   
+
+# ---- Agrupamiento jerárquico  ------
+
+# Parte de las muestras de iris
+datos <- iris[sample(nrow(iris), 50), ]
+
+distancias <- dist(datos[, -5]) # Calculamos las distancias entre muestras
+clusters <- hclust(distancias, method = "average")  # y Generamos la jerarquía de clusters
+
+# Visualización del dendograma
+plot(clusters, labels = datos$Species)
+
+# Análisis de un posible corte con N grupos
+rect.hclust(clusters, k = 4, border = "red")
+
+# Comparar dos soluciones obteniendo estadísticas sobre la mejor
+cluster.stats(distancias, cutree(clusters, 4), cutree(clusters, 5))
+
+# ---- Agrupamiento basado en modelo ----
+library(mclust)
+
+clPairs(iris[, -5], iris$Species)  # Análisis previo de grupos respecto a la clase
+
+modelo <- Mclust(iris[,-5]) # Generación del modelo de acuerdo a parámetros BIC
+summary(modelo)
+table(modelo$classification, iris$Species)  # Muestras por clase en cada cluster
+plot(modelo, what = "classification")
+
+# Modelo mediante análisis discriminante
+modelo <- MclustDA(iris[, -5], iris$Species, modelType = "EDDA")
+summary(modelo)
+plot(modelo, what = "scatterplot")
+
+# Comprobación del error cometido
+unlist(cvMclustDA(modelo, nfold = 10)[2:3])
